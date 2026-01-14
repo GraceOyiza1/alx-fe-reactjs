f5811bd46d23e1568bb22d533ae82b21c784cd80
Grace Oyiza <graceoyiza.dev@gmail.com>
2026-01-14 00:13:03 +0000

    Complete alx-react-app-new with inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ borderTop: '2px solid #ccc', marginTop: '20px', padding: '10px', textAlign: 'center' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
