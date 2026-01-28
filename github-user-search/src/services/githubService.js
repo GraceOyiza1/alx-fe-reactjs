862ec570529022e73ee0a814737274d6a1bd7fb7
Grace Oyiza <graceoyiza.dev@gmail.com>
2026-01-28 18:04:40 +0000

    Moved config and added tailwind tools

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,23 +1,13 @@
 import axios from 'axios';
 
-// The function name must be fetchUserData as per requirements
-export const fetchUserData = async (username) => {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
-    return response.data;
-};
-import axios from 'axios';
-
-export const fetchUserData = async (username, location, minRepos, page = 1) => {
-    // 1. Build the query string
-    // Example query: "user:octocat location:sanfrancisco repos:>10"
+export const fetchUserData = async (username, location, minRepos) => {
+    // We are making a special search link for GitHub
     let query = '';
     if (username) query += `${username}`;
     if (location) query += `+location:${location}`;
     if (minRepos) query += `+repos:>${minRepos}`;
 
-    // 2. Use the Search API endpoint
-    const response = await axios.get(
-        `https://api.github.com/search/users?q=${query}&page=${page}`
-    );
+    // This is the special search address
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
     return response.data;
 };
