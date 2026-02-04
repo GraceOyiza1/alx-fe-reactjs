356b0c10ac1c400ef7fa1fa219ce726579ffe94d
Grace Oyiza <graceoyiza.dev@gmail.com>
2026-02-04 20:14:26 +0000

    fix: add tailwind configuration and directives

--- alx-react-app-new/vite.config.js
+++ alx-react-app-new/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Ensure this is here
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Ensure this is here
+    tailwindcss(),
   ],
 })
